2621140d95257a895c519db404c676549644f780
im-vj <vijay@seabed2crest.com>
2026-06-13 18:19:08 +0530

    feat: add transaction_id column to payments table and implement remote database diagnostic script

--- packages/server/drizzle/0010_conscious_black_bird.sql
+++ packages/server/drizzle/0010_conscious_black_bird.sql
@@ -1,18 +1,4 @@
-ALTER TABLE "cards" ADD COLUMN "user_id" uuid NOT NULL;--> statement-breakpoint
-ALTER TABLE "holders" ADD COLUMN "user_id" uuid NOT NULL;--> statement-breakpoint
 ALTER TABLE "payments" ADD COLUMN "transaction_id" uuid;--> statement-breakpoint
-DO $$ BEGIN
- ALTER TABLE "cards" ADD CONSTRAINT "cards_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE no action ON UPDATE no action;
-EXCEPTION
- WHEN duplicate_object THEN null;
-END $$;
---> statement-breakpoint
-DO $$ BEGIN
- ALTER TABLE "holders" ADD CONSTRAINT "holders_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE no action ON UPDATE no action;
-EXCEPTION
- WHEN duplicate_object THEN null;
-END $$;
---> statement-breakpoint
 DO $$ BEGIN
  ALTER TABLE "payments" ADD CONSTRAINT "payments_transaction_id_transactions_id_fk" FOREIGN KEY ("transaction_id") REFERENCES "transactions"("id") ON DELETE no action ON UPDATE no action;
 EXCEPTION
